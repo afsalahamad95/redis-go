30cb24333316a37b3664a2a86aa48e488ce4c6da
afsalahamada-ac9 <afsalahamad@abovecloud9.ai>
2025-05-04 13:33:15 +0530

    redis basic operations

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // No password set
+		DB:       0,  // Use default DB
+		Protocol: 2,  // Connection protocol
+	})
+
+	ctx := context.Background()
+
+	err := client.Set(ctx, "foo", "bar", 0).Err()
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	val, err := client.Get(ctx, "foo").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("foo", val)
+}
